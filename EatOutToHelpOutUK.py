1fd9232d8db93853bc122ebee8078e794996dd98
Jody Duffy <jody@prodijody.com>
2020-08-05 16:06:26 +0100

    Added use of csv file to open postcodes

--- EatOutToHelpOutUK.py
+++ EatOutToHelpOutUK.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 
 regex_postcode = "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})"
 
+
 def correct_postcode(postcode):
     # returns a cleaned valid postcode if matches regex otherwise returns invalid
     postcode = postcode.replace(" ", "")
@@ -25,11 +26,18 @@ def find_a_restaurant(postcode):
     results = soup.findAll("li", {"class": "govuk-results-list-item"})
     for result in results:
         name = result.findAll("h3", {"class": "govuk-heading-m"})[0].contents[0]
-        address = result.findAll("p", {"class": "govuk-results-address govuk-body"})[0].contents[0]
+        address = result.findAll("p", {"class": "govuk-results-address govuk-body"})[
+            0
+        ].contents[0]
         print(f"{name}: {address}")
-        
+
 
 if __name__ == "__main__":
-    postcode = str(input())
-    postcode = correct_postcode(postcode)
-    find_a_restaurant(postcode)
+    file = open("ab.csv", "rU")
+    reader = csv.reader(file, delimiter=",")
+    count = 0
+    for row in reader:
+        postcode = correct_postcode(row[0])
+        print(postcode)
+        find_a_restaurant(postcode)
+        break
