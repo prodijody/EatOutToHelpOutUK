bee91989e5ed720b8b3d3f0edcd691e12fa50937
Jody Duffy <jody@prodijody.com>
2020-08-05 16:01:41 +0100

    Using bs4

--- EatOutToHelpOutUK.py
+++ EatOutToHelpOutUK.py
@@ -1,43 +1,35 @@
 import requests
 import html
 import csv
+import re
+from bs4 import BeautifulSoup
 
-# The script will ask you to input your post code.
-print("Please enter your post code.")
-print("Please note that spacing is required.")
-print("E.g. 'L39 4QP'\n")
-print('Enter your post code:')
-postcode = input()
-postcode = str(postcode)
+regex_postcode = "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})"
 
-# This is to create a .csv file in the script's directory, with the file name being the post code specified.
-f = open(postcode + '.csv', 'w', newline='')
-csvwriter = csv.writer(f, delimiter=",")
-f.write("Name, Address\n")
+def correct_postcode(postcode):
+    # returns a cleaned valid postcode if matches regex otherwise returns invalid
+    postcode = postcode.replace(" ", "")
+    if re.match(regex_postcode, postcode):
+        postcode = f"{postcode[:-3]} {postcode[-3:]}".upper()
+        return postcode
+    else:
+        return "Invalid"
 
-# The list of registered restaurants within 5 miles of the specified postcode will be obtained from the government website.
-postcode = postcode.split(' ')
-url = 'https://www.tax.service.gov.uk/eat-out-to-help-out/find-a-restaurant/results?postcode=' + \
-    postcode[0] + '+' + postcode[1]
 
-r = requests.get(url)
-r = r.text
+def find_a_restaurant(postcode):
+    # The list of registered restaurants within 5 miles of the specified postcode will be obtained from the government website.
+    url = f"https://www.tax.service.gov.uk/eat-out-to-help-out/find-a-restaurant/results?postcode={postcode}"
 
-# The script will extract the site's data to obtain the restaurant's name and its corresponding address.
-for line in r.splitlines():
-    if '<h3 class="govuk-heading-m"' in line:
-        y = line.split('>')[1]
-        y = y.split('<')[0]
-        y = html.unescape(y)
-    if 'govuk-results-address govuk-body' in line:
-        x = line.split('>')[1]
-        x = x.split('<')[0]
-        x = html.unescape(x)
-        result = (y, x)
-        print(result)
-        csvwriter.writerow(result)
-print('\n')
-print(str(postcode[0] + ' '+postcode[1]) +
-      '.csv has been created on your computer. You may now import the .csv file into google maps.')
-print('If your .csv file is empty, please check if there are indeed participating restaurants in your area.')
-f.close()
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    results = soup.findAll("li", {"class": "govuk-results-list-item"})
+    for result in results:
+        name = result.findAll("h3", {"class": "govuk-heading-m"})[0].contents[0]
+        address = result.findAll("p", {"class": "govuk-results-address govuk-body"})[0].contents[0]
+        print(f"{name}: {address}")
+        
+
+if __name__ == "__main__":
+    postcode = str(input())
+    postcode = correct_postcode(postcode)
+    find_a_restaurant(postcode)
